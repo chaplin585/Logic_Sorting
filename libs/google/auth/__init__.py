901b8b0946fbade87d711031cfe08661f97e2686
chaplin585 <77840364+chaplin585@users.noreply.github.com>
2022-10-14 15:48:07 +0700

    Update __init__.py

--- libs/google/auth/__init__.py
+++ libs/google/auth/__init__.py
@@ -16,8 +16,8 @@
 
 import logging
 
-from libs.google.auth import version as google_auth_version
-from libs.google.auth._default import default, load_credentials_from_file
+from google.auth import version as google_auth_version
+from google.auth._default import default, load_credentials_from_file
 
 
 __version__ = google_auth_version.__version__
